e6c0be3ff454f893cb91b87b9fb2ef53cf7d2ed1
disizaniknem <disizaniknem@gmail.com>
2021-02-26 21:59:04 +0100

    Interface login avec transition de l'image et du texte sur la seconde activité

--- app/src/main/java/com/disizaniknem/uirentcar/MainActivity.java
+++ app/src/main/java/com/disizaniknem/uirentcar/MainActivity.java
@@ -1,10 +1,13 @@
 package com.disizaniknem.uirentcar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Pair;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -42,9 +45,14 @@ public class MainActivity extends AppCompatActivity {
 
         new Handler().postDelayed(() -> {
 
-            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-            startActivity(intent);
-            finish();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+            Pair[] pairs = new Pair[2];
+            pairs[0] = new Pair<View, String>(image, "logo_image");
+            pairs[1] = new Pair<View, String>(title, "logo_text");
+
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+            startActivity(intent, options.toBundle());
 
         }, SPLASH_SCREEN);
 
