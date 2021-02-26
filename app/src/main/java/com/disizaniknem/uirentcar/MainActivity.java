d430f2a3b5c3523fafa6e9ec98a023ac9b9295b5
disizaniknem <disizaniknem@gmail.com>
2021-02-26 21:29:58 +0100

    Splash screen activity

--- app/src/main/java/com/disizaniknem/uirentcar/MainActivity.java
+++ app/src/main/java/com/disizaniknem/uirentcar/MainActivity.java
@@ -2,13 +2,51 @@ package com.disizaniknem.uirentcar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView title, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.ivCar);
+        title = findViewById(R.id.tvTitle);
+        slogan = findViewById(R.id.tvSlogan);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+
+        }, SPLASH_SCREEN);
+
     }
 }
